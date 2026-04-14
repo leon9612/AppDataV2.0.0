9b373e0c83cd11d283b171ff2cba640775293320
Ing Brayan Leon <48490070+leon9612@users.noreply.github.com>
2026-04-14 08:45:00 -0500

    Update eventos.js

--- public/assets/data/eventos.js
+++ public/assets/data/eventos.js
@@ -1,14 +1,4 @@
-const Toast = Swal.mixin({
-        toast: true,
-        position: "top-end",
-        showConfirmButton: false,
-        timer: 5000,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-            toast.onmouseenter = Swal.stopTimer;
-            toast.onmouseleave = Swal.resumeTimer;
-        }
-    });
+
 
 $("#btn-evento").click(function (ev) {
     ev.preventDefault();
